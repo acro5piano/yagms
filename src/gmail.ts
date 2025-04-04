330281ac6ba29bb2b8ba41d2e866c4e147616eb3
Kay Gosho <ketsume0211@gmail.com>
2025-04-04 23:00:46 +0900

    refactor: Update Gmail server to use McpServer and add list-emails tool

--- src/gmail.ts
+++ src/gmail.ts
@@ -1,7 +1,8 @@
-import { Server } from '@modelcontextprotocol/sdk/server/index.js'
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js'
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js'
+import { z } from 'zod'
 
-const server = new Server(
+const server = new McpServer(
   {
     name: 'yagms',
     version: '0.0.1',
@@ -13,3 +14,31 @@ const server = new Server(
     },
   },
 )
+
+// Register weather tools
+server.tool(
+  'list-emails',
+  'Get emails in inbox',
+  {
+    senderEmail: z
+      .string()
+      .email()
+      .describe(
+        'Filter by sender email. Use this to get previous email threads and opponent information.',
+      ),
+  },
+  async ({ senderEmail }) => {
+    // TODO
+  },
+)
+
+async function main() {
+  const transport = new StdioServerTransport()
+  await server.connect(transport)
+  console.error('Weather MCP Server running on stdio')
+}
+
+main().catch((error) => {
+  console.error('Fatal error in main():', error)
+  process.exit(1)
+})
